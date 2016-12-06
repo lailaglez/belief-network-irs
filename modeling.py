8bafdd70d27b2d9be7ff15e123189e00ee9e647a
laila <leylafenix@gmail.com>
2016-12-06 11:39:02 -0500

    Comenzado el trabajo en processing.

--- modeling.py
+++ modeling.py
@@ -4,13 +4,7 @@ import processing
 
 # Este método manda a procesar los datos y crea un índice
 def build(path):
-    names = [name for name in os.listdir(path) if name.endswith('.pdf') or name.endswith('.txt')]
-    processed_data = []
-    for name in names:
-        with open(name, 'rt') as f:
-            # terms será la lista de términos del pdf/txt una vez procesado (quitados los stop words, etc.)
-            terms = processing.process(f)
-            processed_data.append(terms)
+    processing.process(path)
     # create index
 
 
--- processing.py
+++ processing.py
@@ -1,13 +1,68 @@
-# import nltk
-import json
-
+from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus import swadesh
+from nltk.corpus import stopwords
+import nltk
 
 # Procesa un fichero txt/pdf
-def process(f):
+def process(path, language=None):
     terms = []
-    # f = nltk.load(data)
-    # terms = nltk.tokenize(f)
-    # terms = [word for word in terms if word not nltk.stopwordlist]
+    corpus = PlaintextCorpusReader(path, '.*')
+
+    print(corpus.words('10'))
+
+    if not language:
+        language = identify_language(corpus)
+
+    if not language:
+        return None, 'No fue posible detectar el idioma del corpus'
+
+    if language == 'spanish':
+        stopwordlist = stopwords.words('english')
+    elif language == 'english':
+        stopwordlist = stopwords.words('spanish')
+
+    print(language)
+
+    documents = {}
+
+    for c in corpus.fileids():
+        documents[c] = [w for w in corpus.words(c) if w not in stopwordlist]
+        documents[c] = nltk
+
     # terms = nltk.stemming(terms)
     return terms
 
+
+def identify_language(corpus):
+    english_words = swadesh.words('en')
+    spanish_words = swadesh.words('es')
+
+    english_count = 0
+    spanish_count = 0
+
+    for c in corpus.fileids():
+        text = corpus.words(c)
+        freq = nltk.FreqDist(text)
+
+        for key in freq:
+            if key.lower() in english_words:
+                english_count += freq[key]
+            if key.lower() in spanish_words:
+                spanish_count += freq[key]
+
+    if english_count > spanish_count:
+        return 'english'
+    else:
+        return 'spanish'
+
+
+
+
+def unusual_words(text):
+    text_vocab = set(w.lower() for w in text if w.isalpha())
+    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    unusual = text_vocab.difference(english_vocab)
+    return sorted(unusual)
+
+
+print(process('/home/laila/Documents/texts/'))
