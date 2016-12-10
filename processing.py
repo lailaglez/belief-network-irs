8a09830f255dafbae3d67f0f6c8e7dfce89f0ba2
laila <leylafenix@gmail.com>
2016-12-09 19:33:02 -0500

    Agregado procesamiento de la query

--- processing.py
+++ processing.py
@@ -1,12 +1,13 @@
 from nltk.corpus import PlaintextCorpusReader
 from nltk.corpus import swadesh
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet
 import nltk
 import os
 
 
 # Procesa un fichero txt/pdf
-def process(path, language=None):
+def process(path, query, language=None):
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.pdf'):
@@ -30,8 +31,12 @@ def process(path, language=None):
         stemmer = nltk.SnowballStemmer(language)
         documents[c] = [stemmer.stem(w) for w in corpus.words(c) if w not in stopwordlist]
 
-    # Retorna un diccionario {nombre del documento: lista de palabras en el documento}
-    return documents
+    query = [stemmer.stem(q) for q in query if q not in stopwordlist]
+    query = process_query(query)
+
+    # Retorna un diccionario {nombre del documento: lista de términos en el documento}
+    # y una lista con los términos de la query procesada
+    return documents, query
 
 
 language_dic = {'english': 'en', 'spanish': 'es', 'german': 'de', 'french': 'fr', 'italian': 'it', 'portuguese': 'pt'}
@@ -68,6 +73,17 @@ def common_words_ratio(corpus):
             ratio[language] += len(swadesh_set.intersection(word_set))
 
     return ratio
+
+
+def process_query(query):
+    result = list(query)
+
+    for q in query:
+        ss = wordnet.synsets(q)
+        if len(ss) == 1:
+            result += [l.name() for l in ss[0].lemmas()]
+
+    return list(set(result))
 
 
 def unusual_words(text):
