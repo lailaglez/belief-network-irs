0ba1d1ddb96a6e84a30a5e523ec591b5654ed3b5
Jose Gabriel <josripe19@gmail.com>
2017-01-12 14:36:17 -0400

    little change

--- ui.py
+++ ui.py
@@ -8,6 +8,7 @@ import os
 import processing
 import retroalimentation
 
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
